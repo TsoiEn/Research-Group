3b894fe222291aa2c58f61391af8a78f85dc0df3
Renz Aaron <paulanrenz@gmai.com>
2024-09-24 14:25:23 +0800

    chaincode finished

--- Soft_Eng_Research/Blockchain_Core/chaincode/chaincode.go
+++ Soft_Eng_Research/Blockchain_Core/chaincode/chaincode.go
@@ -1,30 +1,118 @@
-package credentials
+package chaincode
 
 import (
-	pb "github.com/hyperledger/fabric/protos/peer"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+// Chaincode definition
 type Student struct {
-	ID         string `json:"id"`
-	FirstName  string `json:"first_name"`
-	LastName   string `json:"last_name"`
-	Age        int    `json:"age"`
-	Birthday   string `json:"birthday"`
-	Credential string `json:"credential"`
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	BirthDate string `json:"birthDate"`
 }
 
-type chaincode interface {
-	Init(stub ChaincodeStubInterface) pb.Response
-	Invoke(stub ChaincodeStubInterface) pb.Response
+type Chaincode struct {
+	contractapi.Contract
 }
 
-type ChaincodeStubInterface interface {
-	InvokeChaincode(chaincodeName string, args [][]byte, channel string) pb.Response
-	GetState(key string) ([]byte, error)
-	PutState(key string, value []byte) error
-	DelState(key string) error
-	GetStateByRange(startKey, endKey string) (StateQueryIteratorInterface, error)
-	GetTxTimestamp() (*timestamp.Timestamp, error)
-	GetTxID() string
-	GetChannelID() string
+// CRUD operations for Student
+
+func (c *Chaincode) AddStudent(ctx contractapi.TransactionContextInterface, student Student) error {
+	exist, err := c.StudentExists(ctx, student.ID)
+	if err != nil {
+		return err
+	}
+
+	if exist {
+		return fmt.Errorf("the student %s already exists", student.ID)
+	}
+
+	Stnd := &Student{
+		ID:        student.ID,
+		FirstName: student.FirstName,
+		LastName:  student.LastName,
+		Age:       student.Age,
+		BirthDate: student.BirthDate,
+	}
+	studentJSON, err := json.Marshal(Stnd)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(student.ID, studentJSON)
+
+}
+
+func (c *Chaincode) ReadStudent(ctx contractapi.TransactionContextInterface, studentID string) (*Student, error) {
+	studentJSON, err := ctx.GetStub().GetState(studentID)
+	if err != nil {
+		return nil, err
+	}
+	if studentJSON == nil {
+		return nil, fmt.Errorf("the student %s does not exist", studentID)
+	}
+
+	var student Student
+	err = json.Unmarshal(studentJSON, &student)
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+}
+
+func (c *Chaincode) UpdateStudent(ctx contractapi.TransactionContextInterface, student Student) error {
+	exist, err := c.StudentExists(ctx, student.ID)
+	if err != nil {
+		return err
+	}
+
+	if !exist {
+		return fmt.Errorf("the student %s does not exist", student.ID)
+	}
+
+	Stnd := &Student{
+		ID:        student.ID,
+		FirstName: student.FirstName,
+		LastName:  student.LastName,
+		Age:       student.Age,
+		BirthDate: student.BirthDate,
+	}
+	studentJSON, err := json.Marshal(Stnd)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(student.ID, studentJSON)
+}
+
+func (c *Chaincode) DeleteStudent(ctx contractapi.TransactionContextInterface, student Student) error {
+	exist, err := c.StudentExists(ctx, student.ID)
+	if err != nil {
+		return err
+	}
+
+	if !exist {
+		return fmt.Errorf("the student %s does not exist", student.ID)
+	}
+
+	stnd := &Student{
+		ID: student.ID,
+	}
+
+	return ctx.GetStub().DelState(stnd.ID)
+}
+
+func (c *Chaincode) StudentExists(ctx contractapi.TransactionContextInterface, studentID string) (bool, error) {
+	studentJSON, err := ctx.GetStub().GetState(studentID)
+	if err != nil {
+		return false, err
+	}
+
+	return studentJSON != nil, nil
 }
