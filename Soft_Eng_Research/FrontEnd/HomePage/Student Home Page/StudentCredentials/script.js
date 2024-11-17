762816812609bf2c90c758f954f5e850d8779927
Yonnewho <142853009+Yonnewho@users.noreply.github.com>
2024-11-18 02:04:50 +0800

    Student Home Page
    Student Profile, Student Credentials

---
+++ Soft_Eng_Research/FrontEnd/HomePage/Student Home Page/StudentCredentials/script.js
@@ -0,0 +1,10 @@
+function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove('active');
+    });
+    document.querySelectorAll('.tab-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+    document.querySelector(`button[onclick="showTab('${tabId}')"]`).classList.add('active');
+}
